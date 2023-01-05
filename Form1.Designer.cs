394f352f551468f8663214d8be53c37acddd873b
_tomenos_ <tomaspial55555@gmail.com>
2023-01-05 13:15:13 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace RegistroTareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 153);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 274);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 89);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escriba tarea completada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tareas Completadas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(250, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Eliminar Selección";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(245, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Abrir en explorador";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(368, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Escriba nueva tarea pendiente";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(368, 153);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(350, 274);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 29);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(350, 89);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(526, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Agregar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(607, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Eliminar Selección";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(918, 612);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Registro de tareas diarias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Label label3;
+        private ListBox listBox2;
+        private TextBox textBox2;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+using System.IO;
+using System.Security.Principal;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Diagnostics;
+
+namespace RegistroTareas
+{
+    public partial class Form1 : Form
+    {
+        //Variables globales
+        
+        static string inputTareaCompletada = null;
+        static string rutaCarpeta = "RegistroTareas";
+
+        static string date = DateTime.Now.ToString("yyyy-MM-dd"); // Asignamos la fecha actual a la variable date
+        static string time = DateTime.Now.ToString("HH:mm:ss"); // Asignamos la hora actual a la variable time
+        static string dateTime = date + "_" + time; // Asignamos la fecha y hora actual a la variable dateTime
+
+        public Form1()
+            
+        {
+            InitializeComponent();
+
+            string rutaArchivo = "RegistroTareas\\" + date + ".txt";
+            if (File.Exists(rutaArchivo))
+            {
+                string[] elementos = File.ReadAllLines(rutaArchivo);
+                listBox1.Items.AddRange(elementos);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e) //Etiqueta del listado de tareas
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e) //Etiqueta del input de tarea completada
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //Campo de texto para ingresar tarea recien completada
+        {
+            inputTareaCompletada = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Botón que añade la tarea del campo de texto
+        {
+            if (inputTareaCompletada == null) // Si el campo de texto está vacío, no se añade nada
+               {
+                MessageBox.Show("No se ha ingresado ninguna tarea");
+            }
+            else // Si el campo de texto está lleno, se añade la tarea al listado
+            {
+                time = DateTime.Now.ToString("HH:mm:ss"); // Asignamos la hora actual a la variable time
+                listBox1.Items.Add(time); //Añadimos la hora actual al listado de tareas
+                listBox1.Items.Add(inputTareaCompletada);
+                textBox1.Text = ""; // Vacía el campo de texto
+
+                // Sobreescritura del archivo de texto con la nueva tarea añadida al listado de tareas 
+                string[] elementosLista = new string[listBox1.Items.Count];
+                listBox1.Items.CopyTo(elementosLista, 0);
+
+                // Creamos la carpeta del registro de tareas si no existe
+                if (!Directory.Exists(rutaCarpeta))
+                {
+                    Directory.CreateDirectory(rutaCarpeta);
+                }
+                string date = DateTime.Now.ToString("yyyy-MM-dd"); // Asignamos la fecha actual a la variable date
+                File.WriteAllLines("RegistroTareas\\" + date + ".txt", elementosLista); // Creamos el archivo de texto con la fecha y hora actual
+            }
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Botón que elimina el elemento seleccionado de la lista
+        {
+            // Preguntamos al usuario con una ventana emergente si está seguro que desea eliminar el elemento seleccionado, si pulsa sobre "No" se cancela la acción
+            DialogResult dialogResult = MessageBox.Show("¿Está seguro que desea eliminar el elemento seleccionado?", "Eliminar elemento", MessageBoxButtons.YesNo);
+
+            // Si el usuario ha pulsado que si, se elimina el elemento seleccionado
+            if (dialogResult == DialogResult.Yes)
+            {
+                // Eliminamos el elemento anterior al que hemos seleccionado en la lista y tambien el que hemos seleccionado
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex - 1);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                
+                    // Sobreescritura de archivo
+                    string[] elementosLista = new string[listBox1.Items.Count];
+                    listBox1.Items.CopyTo(elementosLista, 0);
+
+                    // Creamos la carpeta si no existe
+                    if (!Directory.Exists(rutaCarpeta))
+                    {
+                        Directory.CreateDirectory(rutaCarpeta);
+                    }
+                    string date = DateTime.Now.ToString("yyyy-MM-dd"); // Asignamos la fecha actual a la variable date
+                    File.WriteAllLines("RegistroTareas\\" + date + ".txt", elementosLista); // Creamos el archivo de texto con la fecha y hora actual
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // Abrimos la carpeta del registro de tareas en explorer.exe
+            string rutaCarpeta = "RegistroTareas";
+            Process.Start("explorer.exe", rutaCarpeta);
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }   
+}
