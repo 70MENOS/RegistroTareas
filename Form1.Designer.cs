a3fc7f2b6aa229d171f0b23174c5210da105e4a9
_tomenos_ <tomaspial55555@gmail.com>
2023-01-11 09:32:34 +0100

    mejoras aspecto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,9 +46,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 273);
+            this.listBox1.Location = new System.Drawing.Point(12, 335);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(350, 169);
+            this.listBox1.Size = new System.Drawing.Size(350, 259);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -57,7 +57,7 @@
             this.textBox1.Location = new System.Drawing.Point(12, 29);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(350, 89);
+            this.textBox1.Size = new System.Drawing.Size(350, 61);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -66,7 +66,7 @@
             this.button1.Location = new System.Drawing.Point(369, 58);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Tarea Completada";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -78,26 +78,26 @@
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 17);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Escriba una tarea";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(12, 253);
+            this.label2.Location = new System.Drawing.Point(12, 315);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(114, 17);
-            this.label2.TabIndex = 4;
+            this.label2.TabIndex = 0;
             this.label2.Text = "Tareas Completadas";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(368, 336);
+            this.button2.Location = new System.Drawing.Point(368, 440);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 38);
-            this.button2.TabIndex = 5;
+            this.button2.TabIndex = 6;
             this.button2.Text = "Eliminar Tarea Seleccionada";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -108,7 +108,7 @@
             this.button3.Location = new System.Drawing.Point(368, 87);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(117, 23);
-            this.button3.TabIndex = 6;
+            this.button3.TabIndex = 4;
             this.button3.Text = "Abrir explorador";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -117,10 +117,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Location = new System.Drawing.Point(12, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 17);
-            this.label3.TabIndex = 7;
+            this.label3.TabIndex = 0;
             this.label3.Text = "Tareas Pendientes";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
@@ -129,7 +129,7 @@
             this.button4.Location = new System.Drawing.Point(368, 29);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(117, 23);
-            this.button4.TabIndex = 8;
+            this.button4.TabIndex = 2;
             this.button4.Text = "Tarea Pendiente";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -138,18 +138,18 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(12, 141);
+            this.listBox2.Location = new System.Drawing.Point(12, 113);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(350, 109);
-            this.listBox2.TabIndex = 9;
+            this.listBox2.Size = new System.Drawing.Size(350, 199);
+            this.listBox2.TabIndex = 0;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(368, 169);
+            this.button5.Location = new System.Drawing.Point(368, 188);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(117, 45);
-            this.button5.TabIndex = 10;
+            this.button5.Size = new System.Drawing.Size(117, 40);
+            this.button5.TabIndex = 5;
             this.button5.Text = "Eliminar Tarea Seleccionada";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -159,7 +159,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(490, 447);
+            this.ClientSize = new System.Drawing.Size(490, 597);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.button4);
